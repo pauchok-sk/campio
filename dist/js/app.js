b72458b819d0c974a20981c548234a9f58b46d98
pauchok-sk <pauchoooooook@gmail.com>
2025-01-20 16:35:25 +0300

    repair dist 222

--- dist/js/app.js
+++ dist/js/app.js
@@ -10940,8 +10940,6 @@
                 const scrollTarget = document.getElementById(href);
                 if (scrollTarget) {
                     const offsetPosition = scrollTarget.getBoundingClientRect().top - document.querySelector(".header").clientHeight;
-                    anchors.forEach((a => a.classList.remove("active")));
-                    link.classList.add("active");
                     window.scrollBy({
                         top: offsetPosition,
                         behavior: "smooth"
@@ -11023,6 +11021,27 @@
             }
         }));
     }
+    function profile() {
+        const items = document.querySelectorAll(".profile-sect");
+        const anchorsItems = document.querySelectorAll(".profile__sidebar-btn");
+        const headerHeight = document.querySelector(".header").clientHeight;
+        if (items.length && window.matchMedia("(min-width: 1139px)").matches) {
+            let currentIndex = 0;
+            window.addEventListener("scroll", (e => {
+                handlerScroll(currentIndex);
+            }));
+            function handlerScroll(index) {
+                if (index <= items.length - 1) {
+                    const rectTop = items[index].getBoundingClientRect().top - headerHeight;
+                    const top = rectTop + document.documentElement.scrollTop;
+                    if (window.scrollY >= top && window.scrollY <= top + items[index].clientHeight) {
+                        anchorsItems.forEach((a => a.classList.remove("active")));
+                        anchorsItems[index].classList.add("active");
+                    } else handlerScroll(index + 1);
+                }
+            }
+        }
+    }
     mediaAdaptive();
     spoller();
     slider();
@@ -11046,4 +11065,5 @@
     prevBtn();
     mask();
     email();
+    profile();
 })();
